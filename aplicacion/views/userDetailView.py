b75d8941e51f317b705fda45b9c4c52b62f942a2
pipe <pipe@gmail.com>
2022-09-23 06:18:50 -0500

    agregando userview

--- aplicacion/views/userDetailView.py
+++ aplicacion/views/userDetailView.py
@@ -5,20 +5,24 @@ from rest_framework_simplejwt.backends import TokenBackend
 from rest_framework.permissions import IsAuthenticated
 from aplicacion.models.user import User
 from aplicacion.serializers.userSerializer import UserSerializer
+from rest_framework.decorators import api_view
+
 
 class UserDetailView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (IsAuthenticated,)
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):     
 
         token = request.META.get('HTTP_AUTHORIZATION')[7:]
         tokenBackend = TokenBackend(algorithm=settings.SIMPLE_JWT['ALGORITHM'])
         valid_data = tokenBackend.decode(token,verify=False)
         
-        if valid_data['user_id'] != kwargs['pk']:
-            stringResponse = {'detail':'Unauthorized Request'}
-            return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
+       # if valid_data['user_id'] != kwargs['pk']:
+        #    stringResponse = {'detail':'Unauthorized Request'}
+         #   return Response(stringResponse, status=status.HTTP_401_UNAUTHORIZED)
 
-        return super().get(request, *args, **kwargs)
+        return super().get(request, *args, **kwargs)    
+
+
