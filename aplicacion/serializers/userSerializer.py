99f881e160df116d6e6a17c325c1d27c05a73f23
pipe <pipe@gmail.com>
2022-09-23 08:55:14 -0500

    con tabla user heroku

--- aplicacion/serializers/userSerializer.py
+++ aplicacion/serializers/userSerializer.py
@@ -6,29 +6,3 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User        
         fields = ['id', 'username', 'password', 'name', 'idCard', 'address', 'phone','email']
-<<<<<<< HEAD
-        
-=======
->>>>>>> e1d122693a285883ccf6ff4ff2eea0cd35168040
-        
-''' 
-    def create(self, validated_data):
-        productosData = validated_data.pop('productos')
-        userInstance = User.objects.create(**validated_data)
-        Productos.objects.create(user=userInstance, **productosData)
-        return userInstance
-    
-    def to_representation(self, obj):
-      user = User.objects.get(id=obj.id)
-      productos = Productos.objects.get(user=obj.id)
-      return{
-          'id': user.id,
-          'username': user.username,
-          'name': user.name,
-          'idCard': user.idCard,
-          'address': user.address,
-          'phone': user.phone,
-          'email': user.email      
-          
-      }         
-''' 
--- aplicacion/views/userView.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from aplicacion.serializers import UserSerializer
-from aplicacion.models import User
-#from rest_framework.permissions import IsAuthenticated
-
-class UserView(viewsets.ModelViewSet):
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-   #permission_classes = (IsAuthenticated,)
